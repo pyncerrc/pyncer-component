3e1289570f41a638985d9cc8c92beb78a8413676
pyncer <root@pyncer.com>
2023-05-24 12:54:53 -0400

    Added missing functions to component interface

--- src/ComponentInterface.php
+++ src/ComponentInterface.php
@@ -1,10 +1,18 @@
 <?php
 namespace Pyncer\Component;
 
+use Psr\Http\Message\RequestInterface as PsrRequestInterface;
 use Pyncer\Container\ContainerInterface;
+use Pyncer\Http\Message\RequestData;
 use Pyncer\Http\Server\RequestResponseInterface;
 
 interface ComponentInterface extends
     ContainerInterface,
     RequestResponseInterface
-{}
+{
+    public function getRequest(): PsrRequestInterface;
+
+    public function getQueryParams(): RequestData;
+
+    public function getParsedBody(): RequestData;
+}
